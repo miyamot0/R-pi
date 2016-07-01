a872c26cb9645a87185d191079ba70a16da914e7
Shawn Gilroy <shawnpgilroy@gmail.com>
2016-07-01 13:17:02 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
-  require_once 'Config.php';
-  require 'Connection.php';
+	require_once 'Config.php';
+	require 'Connection.php';
 
 	$cnx = new Rserve_Connection(RSERVE_HOST,
 	    RSERVE_PORT,
 	    array('username'=>RSERVE_USER,'password'=>RSERVE_PASS)
 	);
 
-  $resp = $cnx->evalString('x = "Server up"', Rserve_Connection::PARSER_NATIVE);
+	  $resp = $cnx->evalString('x = "Server up"', Rserve_Connection::PARSER_NATIVE);
   
-  echo $resp;
+	  echo $resp;
 ?>
